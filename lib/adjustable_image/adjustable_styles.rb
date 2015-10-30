99095966db1d302e3107b87972e795b30d0c2279
Rebecca Krathwohl <rkrathwohl@charitybuzz.com>
2015-10-30 15:39:05 -0400

    Add dynamic styles method to rearrange the styles passed to paperclip

--- lib/adjustable_image/adjustable_styles.rb
+++ lib/adjustable_image/adjustable_styles.rb
@@ -1,8 +1,72 @@
 module AdjustableImage
   module AdjustableStyles
 
+    BASE_STYLE_PROCESSORS = [:dynamic_resize_processor, :background_color_processor, :dynamic_extent_processor]
+    NON_BASE_STYLE_PROCESSORS = [:reset_base_style_processor]
+
     def dynamic_styles
-      {}
+      if self.new_record?
+        style_for_new_instance
+      elsif self.image_adjustments.has_adjustments?
+        adjusted_styles
+      else
+        {}
+      end
+    end
+
+    private
+
+    def style_for_new_instance
+      if self.image_style_options[:force_actual_format].present?
+        { original: { processors: [:force_actual_format_processor] } }
+      else
+        {}
+      end
+    end
+
+    def adjusted_styles
+      adjusted_styles = ActiveSupport::OrderedHash.new
+
+      if base_style_name.present?
+        adjusted_styles[base_style_name] = adjusted_base_style_hash
+
+        self.image_style_options[:static_styles].each_pair do |style_name, style_hash|
+          next if style_name == base_style_name
+          adjusted_styles[style_name] = adjusted_style_hash_for(style_name, style_hash)
+        end
+      end
+
+      adjusted_styles
+    end
+
+    def adjusted_base_style_hash
+      base_style = self.image_style_options[:static_styles][base_style_name] || {}
+
+      all_processors = add_necessary_processors(base_style, BASE_STYLE_PROCESSORS)
+
+      base_style.merge(self.image_adjustments.styles_hash).merge(processors: all_processors)
+    end
+
+    def adjusted_style_hash_for(style_name, style_hash)
+      all_processors = add_necessary_processors(style_hash, NON_BASE_STYLE_PROCESSORS)
+
+      style_hash.merge(processors: all_processors, base_style_name: base_style_name)
+    end
+
+    def add_necessary_processors(style_hash, adjusting_processors)
+      if (style_processors = style_hash[:processors]).present?
+        adjusting_processors + style_processors
+      else
+        adjusting_processors + default_processors
+      end
+    end
+
+    def default_processors
+      self.image_style_options[:default_processors] || []
+    end
+
+    def base_style_name
+      self.image_style_options[:base_style_name]
     end
 
   end
--- spec/adjustable_image/adjustable_styles_spec.rb
+++ spec/adjustable_image/adjustable_styles_spec.rb
@@ -3,5 +3,219 @@ require 'spec_helper'
 describe AdjustableImage::AdjustableStyles do
 
   describe '#dynamic_styles' do
+    context 'when the attached-to object is a new record' do
+      let(:subject) { StylesContainer.new(image_style_options, nil, true) }
+
+      context 'when the force_actual_format option is true' do
+        let(:image_style_options) { { force_actual_format: true } }
+
+        it 'returns a hash with a processor for the original style' do
+          expect(subject.dynamic_styles).to eq({ original: { processors: [:force_actual_format_processor] } })
+        end
+      end
+
+      context 'when the force_actual_format option is false' do
+        let(:image_style_options) { {} }
+
+        it 'returns an empty hash' do
+          expect(subject.dynamic_styles).to eq({})
+        end
+      end
+    end
+
+    context 'when the attached-to object is not a new record' do
+      context 'with no existing image adjustments' do
+        let(:subject) { StylesContainer.new({}, nil, false) }
+
+        it 'returns an empty hash' do
+          expect(subject.dynamic_styles).to eq({})
+        end
+      end
+
+      context 'with existing image adjustments' do
+        let(:image_adjustments) { ImageAdjustments.new(crop_x: 1, crop_y: 1, crop_width: 1, crop_height: 1,
+                                                       resize_width_to: 1, resize_height_to: 1) }
+
+        context 'with no base style name' do
+          let(:subject) { StylesContainer.new({}, image_adjustments, false) }
+
+          it 'returns an empty hash' do
+            expect(subject.dynamic_styles).to eq({})
+          end
+        end
+
+        context 'with a base style name' do
+          let(:adjusted_styles) { subject.dynamic_styles }
+          let(:subject) { StylesContainer.new(image_style_options, image_adjustments, false) }
+
+          let(:base_style_name) { :teapot_size }
+
+          describe 'base style' do
+            describe 'processors' do
+              let(:base_image_processors) { adjusted_styles[base_style_name][:processors] }
+
+              context 'with default processors' do
+                context 'with no style specific processors' do
+                  let(:image_style_options) do
+                    { base_style_name: base_style_name,
+                      static_styles: { teacup_size: { geometry: '2x2' } },
+                      default_processors: [:make_smaller] }
+                  end
+
+                  it 'adds the base style processors before the options processors' do
+                    expect(base_image_processors).to eq(AdjustableImage::AdjustableStyles::BASE_STYLE_PROCESSORS + [:make_smaller])
+                  end
+                end
+
+                context 'with style specific processors' do
+                  let(:image_style_options) do
+                    { base_style_name: base_style_name,
+                      static_styles: { base_style_name => { processors: [:make_teapot_size] },
+                                       teacup_size: { geometry: '2x2' } },
+                      default_processors: [:make_smaller] }
+                  end
+
+                  it 'adds the base style processors before the style specific processors' do
+                    expect(base_image_processors).to eq(AdjustableImage::AdjustableStyles::BASE_STYLE_PROCESSORS + [:make_teapot_size])
+                  end
+                end
+              end
+
+              context 'with no default processors' do
+                context 'with style specific processors' do
+                  let(:image_style_options) do
+                    { base_style_name: base_style_name,
+                      static_styles: { base_style_name => { processors: [:make_kettle_size] },
+                                       teacup_size: { geometry: '2x2' } } }
+                  end
+
+                  it 'adds the base style processors before the style specific processors' do
+                    expect(base_image_processors).to eq(AdjustableImage::AdjustableStyles::BASE_STYLE_PROCESSORS + [:make_kettle_size])
+                  end
+                end
+              end
+
+              context 'with no processors' do
+                let(:image_style_options) do
+                  { base_style_name: base_style_name,
+                    static_styles: { teacup_size: { geometry: '2x2' } } }
+                end
+
+                it 'adds the base style processors' do
+                  expect(base_image_processors).to eq(AdjustableImage::AdjustableStyles::BASE_STYLE_PROCESSORS)
+                end
+              end
+            end
+
+            describe 'styles' do
+              let(:image_style_options) do
+                { base_style_name: base_style_name,
+                  static_styles: { base_style_name => { geometry: '120x120' },
+                                   teacup_size: { geometry: '2x2' } } }
+              end
+
+              let(:base_image_styles) { adjusted_styles[base_style_name] }
+
+              it 'adds the styles from the image adjustment' do
+                base_image_styles.delete(:processors)
+
+                expect(base_image_styles).to eq({ crop_x: 1, crop_y: 1,
+                                                  crop_width: 1, crop_height: 1,
+                                                  resize_width_to: 1, resize_height_to: 1,
+                                                  background_color: 'FFFFFF', geometry: '1x1' })
+              end
+            end
+          end
+
+          describe 'all other styles' do
+            describe 'processors' do
+              let(:non_base_processors) { AdjustableImage::AdjustableStyles::NON_BASE_STYLE_PROCESSORS }
+
+              context 'with default processors' do
+                let(:image_style_options) do
+                  { base_style_name: base_style_name,
+                    static_styles: {
+                      teacup: { geometry: '15x15' },
+                      mug: { geometry: '72x72', processors: [:make_tea_sized] }
+                    },
+                    default_processors: [:make_larger] }
+                end
+
+                it 'adds the non-base style processors before the options processors' do
+                  teacup_processors = adjusted_styles[:teacup][:processors]
+
+                  expect(teacup_processors).to eq(non_base_processors + [:make_larger])
+                end
+
+                context 'with style specific processors' do
+                  it 'adds the non-base style processors before the style specific processors' do
+                    mug_processors = adjusted_styles[:mug][:processors]
+
+                    expect(mug_processors).to eq(non_base_processors + [:make_tea_sized])
+                  end
+                end
+              end
+
+              context 'without default processors' do
+                context 'with style specific processors' do
+                  let(:image_style_options) do
+                    {
+                      base_style_name: base_style_name,
+                      static_styles: { mug: { geometry: '71x71', processors: [:make_cup_sized] } }
+                    }
+                  end
+
+                  it 'adds the non-base style processors before the style specific processors' do
+                    expect(adjusted_styles[:mug][:processors]).to eq(non_base_processors + [:make_cup_sized])
+                  end
+                end
+              end
+
+              context 'with no processors' do
+                let(:image_style_options) do
+                  { base_style_name: base_style_name, static_styles: { mug: { geometry: '17x17' } } }
+                end
+
+                it 'adds the non-base style processors' do
+                  expect(adjusted_styles[:mug][:processors]).to eq(non_base_processors)
+                end
+              end
+            end
+
+            describe 'styles' do
+              let(:image_style_options) do
+                { base_style_name: base_style_name,
+                  static_styles: {
+                    coffee_mug: { geometry: '50x51' },
+                  } }
+              end
+
+              it 'returns the passed-in style for that style name' do
+                coffee_mug_styles = adjusted_styles[:coffee_mug]
+                coffee_mug_styles.delete(:processors)
+
+                expect(coffee_mug_styles).to eq(geometry: '50x51', base_style_name: base_style_name)
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+class StylesContainer
+  include AdjustableImage::AdjustableStyles
+
+  attr_accessor :image_style_options, :image_adjustments, :new_record
+
+  def initialize(options, adjustments, new_record)
+    self.image_style_options = options
+    self.image_adjustments = adjustments || ImageAdjustments.new
+    self.new_record = new_record
+  end
+
+  def new_record?
+    self.new_record
   end
 end
